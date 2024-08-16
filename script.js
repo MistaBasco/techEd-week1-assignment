c9c9779c858d4302bb2de2108b2f21b903a25922
MistaBasco <mistabasco>
2024-08-16 16:40:23 +0100

    added onmousover and onmouseout inline js for all social-links in footer. Wooooo, colourgit add .

--- script.js
+++ script.js
@@ -5,3 +5,10 @@
 //   widget.setVolume(0.5); // Set volume to 50%
 //   widget.play(); // Start playing
 // };
+
+// https://img.icons8.com/color/48/twitter--v1.png
+// https://img.icons8.com/fluency/48/instagram-new.png
+// https://img.icons8.com/color/48/facebook.png
+// https://img.icons8.com/color/48/tiktok--v1.png
+// https://img.icons8.com/color/48/linkedin.png
+// https://img.icons8.com/color/48/youtube-play.png
