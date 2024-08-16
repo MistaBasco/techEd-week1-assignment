febb59808d4bf3bcbfd1be3736dbaf2bdace146f
MistaBasco <mistabasco>
2024-08-16 15:51:58 +0100

    removed js because no longer needed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-window.onload = function () {
-  var iframeElement = document.getElementById("soundcloud-player");
-  var widget = SC.Widget(iframeElement);
+// window.onload = function () {
+//   var iframeElement = document.getElementById("soundcloud-player");
+//   var widget = SC.Widget(iframeElement);
 
-  widget.setVolume(0.5); // Set volume to 50%
-  widget.play(); // Start playing
-};
+//   widget.setVolume(0.5); // Set volume to 50%
+//   widget.play(); // Start playing
+// };
