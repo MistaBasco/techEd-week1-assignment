a135ca08362c9c7cc0026516a7eefc0be58b2a41
MistaBasco <mistabasco>
2024-08-16 14:42:46 +0100

    changed default play volume from 0.3 to 0.5

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ window.onload = function () {
   var iframeElement = document.getElementById("soundcloud-player");
   var widget = SC.Widget(iframeElement);
 
-  widget.setVolume(0.3); // Set volume to 50%
+  widget.setVolume(0.5); // Set volume to 50%
   widget.play(); // Start playing
 };
