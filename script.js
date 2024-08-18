d9831c45e3d766192a17621a3c9410f0b504be91
MistaBasco <mistabasco>
2024-08-18 17:29:12 +0100

    Used chatGpt as an assist to replace the inline js with a re-usable function for all the social-link hover events. learned about custom element attributes using data- and how to access them within js

--- script.js
+++ script.js
@@ -12,3 +12,25 @@
 // https://img.icons8.com/color/48/tiktok--v1.png
 // https://img.icons8.com/color/48/linkedin.png
 // https://img.icons8.com/color/48/youtube-play.png
+
+function iconHover(imgElement, hoverImg) {
+  const nonHoverImg = imgElement.src;
+
+  imgElement.addEventListener("mouseover", function () {
+    imgElement.src = hoverImg;
+  });
+
+  imgElement.addEventListener("mouseout", function () {
+    imgElement.src = nonHoverImg;
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Select all images you want to apply the hover effect to
+  const images = document.querySelectorAll(".hover-img");
+
+  images.forEach(function (img) {
+    // Call the iconHover function for each image
+    iconHover(img, img.getAttribute("data-hover-src"));
+  });
+});
